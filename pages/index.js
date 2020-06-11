132b45f0e7aea6c01cefe91785194bbd697f7cf1
farnam <farnam1997@gmail.com>
2020-06-11 22:18:27 +0430

    add server.js and routing with next js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import Link from 'next/link'
+import { bindActionCreators } from 'redux';
+import { initStore, initialCards, addItem } from '../store';
+import withRedux from 'next-redux-wrapper';
 import './index.css';
 import Card from './Card';
-import data from '../data/data.json';
 
-export default class Index extends React.Component  {
-    static async getInitialProps () {
-        return { cards: data }
+class Index extends React.Component {
+    static async getInitialProps({ store }) {
+        store.dispatch(initialCards());
     }
 
-    render () {
+    render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src="/static/logo.png"
-                        className="static-logo" alt="logo"
-                    />
+                    <Link href='/page3'>
+                        <img src="/static/logo.png"
+                            className="static-logo" alt="logo"
+                        />
+                    </Link>
+
                 </header>
                 <div className="Grid">
                     {
@@ -28,3 +34,18 @@ export default class Index extends React.Component  {
     }
 
 };
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        initialCards: bindActionCreators(initialCards, dispatch),
+        addItem: bindActionCreators(addItem, dispatch)
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        cards: state.cards,
+    }
+}
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index);
--- store.js
+++ store.js
@@ -1,19 +1,45 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import ThunkMiddleware from 'redux-thunk'
-import data from './data/data.json'
-
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import data from './data/data.json';
 
 // initial state
-
-
 const startState = {
     cards: []
 }
 
-// create store
+// Actions
+export const initialCards = () => {
+    return {
+      type: 'INITIALCARDS',
+      cards: data 
+    }
+}
 
+export const addItem = (item) => {
+    return {
+        type: 'ADD',
+        item
+    }
+}
+
+// reducers
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INITIALCARDS':
+            return {
+                cards: action.cards,
+            }
+        case 'ADD':
+            return {
+                ...state,
+                cards: [...state.cards, action.item]
+            }
+        default: return state
+    }
+}
+
+// create store
 export const initStore = (initialState = startState) => {
-    return createStore(reducer, initialState,
-        composeWithDevTools(applyMiddleware(ThunkMiddleware)))
+    return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 }
